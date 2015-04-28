280d1ca0aedfa5b40e2d61c0af55c7e43d4c5e76
Svetlana Filimonova <sfilimonova@me.com>
2015-04-28 23:02:20 +1000

    Read and assert git status. Just to demo the idea. Please dont judge me

--- README.md
+++ README.md
@@ -10,3 +10,12 @@ Ygitme is a git commands-challenger
 3. Commit your changes (`git commit -am 'Add some feature'`)
 4. Push to the branch (`git push origin my-new-feature`)
 5. Create a new Pull Request
+
+## Developing
+1. Change stuff, write some tests
+2. rake install
+
+## Publishing
+1. Bump the lib/ygitme/version.rb
+2. gem build
+3. gem publish ygitme-...
--- lib/ygitme.rb
+++ lib/ygitme.rb
@@ -1,9 +1,19 @@
 require "ygitme/version"
 
 module Ygitme  
-  class Hello
-    def say_hello
-      puts 'This is ygitme. Please start hacking.'
+  class Challenge
+    def gitme
+      gitStatus
+    end
+
+    def gitStatus
+      puts 'Y git me a status of things'
+      gitStatus = gets.chomp
+      if (gitStatus == 'git status')
+        puts 'Yahoo, you are right!'
+      else
+        puts 'Y need to git MOAR!'
+      end
     end
   end
 end  
